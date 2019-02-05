692f948c78c45d9f0246c321e2371d417589f152
Danilo Pereira <danilo-gc@hotmail.com>
2019-02-05 14:52:45 -0300

    começando a definir o layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SettingsComponent } from './settings/settings.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
